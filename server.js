f6c3521633177435de9763d2004849463b6598b9
Kan5661 <linkan0718@gmail.com>
2022-12-20 11:11:46 -0500

    api get

--- server.js
+++ server.js
@@ -1,15 +1,35 @@
 import express from 'express'
 import fetch from 'node-fetch'
+import mongoose from 'mongoose'
+import connection from './connection.js'
+import Brawler from './models/Brawler.js'
+import Mode from './models/Mode.js'
+import Event from './models/Event.js'
+import Map from './models/Map.js'
 
 const app = express()
 app.use(express.json())
 
-const PORT = 3500
+const PORT = 3400
 
 app.listen(PORT, () => {
     console.log('listening to port 3500')
 })
 
 app.get('/', (req, res) => {
-    console.log('hello world')
+    res.send('hello world')
+})
+
+// GET
+app.get('/brawlers', async (req, res) => {
+    res.json(await Brawler.find({}))
+})
+app.get('/modes', async (req, res) => {
+    res.json(await Mode.find({}))
+})
+app.get('/events', async (req, res) => {
+    res.json(await Event.find({}))
+})
+app.get('/maps', async (req, res) => {
+    res.json(await Map.find({}))
 })
