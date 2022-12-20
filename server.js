d6b5f38c64fb790a2623291357e049ce3379e18d
Kan5661 <linkan0718@gmail.com>
2022-12-20 14:29:21 -0500

    api post

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ app.use(express.json())
 const PORT = 3400
 
 app.listen(PORT, () => {
-    console.log('listening to port 3500')
+    console.log('listening to port 3400')
 })
 
 app.get('/', (req, res) => {
@@ -32,4 +32,38 @@ app.get('/events', async (req, res) => {
 })
 app.get('/maps', async (req, res) => {
     res.json(await Map.find({}))
+})
+
+// POST 
+app.post('/maps', async (req, res) => {
+    res.json(await Map.findOneAndUpdate(
+        {},
+        { $push: {list: req.body}}
+    ))
+})
+app.post('/events', async (req, res) => {
+    res.json(await Event.findOneAndUpdate(
+        {},
+        { $push: {list: req.body}}
+    ))
+})
+app.post('/modes', async (req, res) => {
+    res.json(await Mode.findOneAndUpdate(
+        {},
+        { $push: {active: req.body}}
+    ))
+})
+app.post('/brawlers', async (req, res) => {
+    res.json(await Brawler.findOneAndUpdate(
+        {},
+        { $push: {list: req.body}}
+    ))
+})
+
+// DELETE
+
+app.delete('/maps/:id',  async (req, res) => {
+    Map.findOneAndRemove(
+        {_id: req.params.id}
+    )
 })
